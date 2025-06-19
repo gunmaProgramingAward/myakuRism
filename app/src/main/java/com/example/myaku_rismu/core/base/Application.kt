f61a8ff04fa487c14e04c3e3f4ae8ca27bd6bea6
REOysd <202yyrr@gmail.com>
2025-06-19 23:12:11 +0900

    daggarHiltの追加

---
+++ app/src/main/java/com/example/myaku_rismu/core/base/Application.kt
@@ -0,0 +1,8 @@
+package com.example.myaku_rismu.core.base
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class MyakuRismuApplication : Application() {
+}
--- app/src/main/java/com/example/myaku_rismu/core/base/MainActivity.kt
+++ app/src/main/java/com/example/myaku_rismu/core/base/MainActivity.kt
@@ -12,7 +12,9 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.myaku_rismu.ui.theme.Myaku_rismuTheme
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
