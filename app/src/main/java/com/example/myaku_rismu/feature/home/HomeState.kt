5a7d31bf74cb1880c5d5aa1313e237839ac78f4e
REOysd <202yyrr@gmail.com>
2025-06-19 22:07:20 +0900

    UiStateの実装

---
+++ app/src/main/java/com/example/myaku_rismu/feature/home/HomeState.kt
@@ -0,0 +1,7 @@
+package com.example.myaku_rismu.feature.home
+
+import com.example.myaku_rismu.core.ScreenState
+
+data class HomeState(
+    val screenState: ScreenState = ScreenState.Initializing()
+)
