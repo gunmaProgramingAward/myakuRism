149fbc8a43dc177127f65e532a8cacae8eb12e9d
Shunsuke Uchimura <25cm0105@jec.ac.jp>
2025-07-06 15:09:44 +0900

    状態変数群の追加とBpmPlayerCardの修正

--- app/src/main/java/com/example/myaku_rismu/feature/home/HomeScreen.kt
+++ app/src/main/java/com/example/myaku_rismu/feature/home/HomeScreen.kt
@@ -2,17 +2,25 @@ package com.example.myaku_rismu.feature.home
 
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -22,34 +30,120 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.myaku_rismu.R
+import com.example.myaku_rismu.feature.home.components.BarChart
+import com.example.myaku_rismu.feature.home.components.DonutChart
 import com.example.myaku_rismu.ui.theme.Myaku_rismuTheme
 import com.example.myaku_rismu.ui.theme.customTheme
 
 @Composable
 fun HomeScreen(modifier: Modifier = Modifier) {
 
-    var bpmLevel by remember { mutableIntStateOf(0) }
-    var bpmCount by remember { mutableIntStateOf(0) }
+    // --- 心拍数の表示値　---
+    var bpmPlayerValue by remember { mutableIntStateOf(0) }
+    // --- 各健康メトリックの値 ---
+    var metricHeartRateCurrentValue by remember { mutableIntStateOf(0) }
+    var metricStepsCurrentValue by remember { mutableIntStateOf(0) }
+    var metricMoveCurrentValue by remember { mutableIntStateOf(0) }
+    var metricSleepCurrentValue by remember { mutableIntStateOf(0) }
+    var metricDistanceCurrentValue by remember { mutableIntStateOf(0) }
+    // --- 各健康メトリックの目標値 ---
+    var metricHeartRateTargetValue by remember { mutableIntStateOf(100) }
+    var metricStepsTargetValue by remember { mutableIntStateOf(10000) }
+    var metricMoveTargetValue by remember { mutableIntStateOf(2000) }
+    var metricSleepTargetValue by remember { mutableIntStateOf(8) }
+    var metricDistanceTargetValue by remember { mutableIntStateOf(5) }
 
-    val bpmColor = when (bpmLevel) {
-        1 -> MaterialTheme.customTheme.homeMediumBpmColor
-        2 -> MaterialTheme.customTheme.homeHighBpmColor
+    val bpmPlayerColor = when (bpmPlayerValue) {
+        in 81..140 -> MaterialTheme.customTheme.homeMediumBpmColor
+        in 140..300 -> MaterialTheme.customTheme.homeHighBpmColor
         else -> MaterialTheme.customTheme.homeLowBpmColor
     }
 
+    // --- 各健康メトリックの色とバーの色(dataクラスにまとめるか検討中) ---
+    val homeHeartRateColor = MaterialTheme.customTheme.homeHeartRateColor
+    val homeStepsColor = MaterialTheme.customTheme.homeStepsColor
+    val homeMoveColor = MaterialTheme.customTheme.homeMoveColor
+    val homeSleepColor = MaterialTheme.customTheme.homeSleepColor
+    val homeDistanceColor = MaterialTheme.customTheme.homeDistanceColor
+    val homeHeartRateBarColorFaded = MaterialTheme.customTheme.homeHeartRateBarColorFaded
+    val homeStepsBarColorFaded = MaterialTheme.customTheme.homeStepsBarColorFaded
+    val homeMoveBarColorFaded = MaterialTheme.customTheme.homeMoveBarColorFaded
+    val homeSleepBarColorFaded = MaterialTheme.customTheme.homeSleepBarColorFaded
+    val homeDistanceBarColorFaded = MaterialTheme.customTheme.homeDistanceBarColorFaded
+
+
+    val healthMetricsData = remember(
+        metricHeartRateCurrentValue, metricHeartRateTargetValue,
+        metricStepsCurrentValue, metricStepsTargetValue,
+        metricMoveCurrentValue, metricMoveTargetValue,
+        metricSleepCurrentValue, metricSleepTargetValue,
+        metricDistanceCurrentValue, metricDistanceTargetValue
+    ) {
+        listOf(
+            HealthMetric(
+                titleResId = (R.string.current_heart_rate),
+                iconResId = R.drawable.heartrate,
+                cardThemeColor = homeHeartRateColor,
+                barColorFaded = homeHeartRateBarColorFaded,
+                currentValue = metricHeartRateCurrentValue,
+                targetValue = metricHeartRateTargetValue,
+                unitResId = R.string.unit_bpm
+            ),
+            HealthMetric(
+                titleResId = (R.string.steps),
+                iconResId = R.drawable.steps,
+                cardThemeColor = homeStepsColor,
+                barColorFaded = homeStepsBarColorFaded,
+                currentValue = metricStepsCurrentValue,
+                targetValue = metricStepsTargetValue,
+                unitResId = null,
+            ),
+            HealthMetric(
+                titleResId = (R.string.move),
+                iconResId = R.drawable.move,
+                cardThemeColor = homeMoveColor,
+                barColorFaded = homeMoveBarColorFaded,
+                currentValue = metricMoveCurrentValue,
+                targetValue = metricMoveTargetValue,
+                unitResId = R.string.unit_kcal,
+            ),
+            HealthMetric(
+                titleResId = (R.string.sleep),
+                iconResId = R.drawable.sleep,
+                cardThemeColor = homeSleepColor,
+                barColorFaded = homeSleepBarColorFaded,
+                currentValue = metricSleepCurrentValue,
+                targetValue = metricSleepTargetValue,
+                unitResId = R.string.unit_hours,
+            ),
+            HealthMetric(
+                titleResId = (R.string.distance),
+                iconResId = R.drawable.distance,
+                cardThemeColor = homeDistanceColor,
+                barColorFaded = homeDistanceBarColorFaded,
+                currentValue = metricDistanceCurrentValue,
+                targetValue = metricDistanceTargetValue,
+                unitResId = R.string.unit_km,
+            )
+        )
+    }
+
+
+// --- メインの画面レイアウト ---
     Column(
         modifier = modifier.fillMaxSize()
     ) {
         BpmPlayerCard(
             modifier = Modifier,
-            bpmCount = bpmCount,
-            bpmColor = bpmColor
+            bpmCount = bpmPlayerValue,
+            bpmColor = bpmPlayerColor
         )
     }
 }
@@ -59,7 +153,7 @@ fun HomeScreen(modifier: Modifier = Modifier) {
 fun BpmPlayerCard(
     modifier: Modifier = Modifier,
     bpmCount: Int,
-    bpmColor: androidx.compose.ui.graphics.Color
+    bpmColor: Color
 ) {
     Box(
         modifier = modifier
@@ -113,6 +207,9 @@ fun BpmPlayerCard(
 }
 
 
+
+
+
 @Preview(showBackground = true)
 @Composable
 fun HomeScreenPreview() {
@@ -121,10 +218,4 @@ fun HomeScreenPreview() {
     }
 }
 
-@Preview(showBackground = true)
-@Composable
-fun BpmPlayerCardPreview() {
-    Myaku_rismuTheme {
-        BpmPlayerCard(bpmCount = 174, bpmColor = MaterialTheme.customTheme.homeMediumBpmColor)
-    }
-}
+
