db7b96ae9485bfbf89d362cdb1e476b8b0a6cd27
Shunsuke Uchimura <25cm0105@jec.ac.jp>
2025-07-16 23:29:37 +0900

    UiState/ViewModel/UiEventの実装

--- app/src/main/java/com/example/myaku_rismu/feature/setting/SettingUiEvent.kt
+++ app/src/main/java/com/example/myaku_rismu/feature/setting/SettingUiEvent.kt
@@ -3,4 +3,14 @@ package com.example.myaku_rismu.feature.setting
 import com.example.myaku_rismu.core.UiEvent
 
 sealed interface SettingUiEvent : UiEvent {
+    data object ShowBirthdateDialog : SettingUiEvent
+    data object ShowHeightDialog : SettingUiEvent
+    data object ShowWeightDialog : SettingUiEvent
+    data object ShowGenderDialog : SettingUiEvent
+    data object DismissDialog : SettingUiEvent
+    data class BirthdateSelected(val year: Int, val month: Int, val day: Int) : SettingUiEvent
+    data class HeightSelected(val height: Int) : SettingUiEvent
+    data class WeightSelected(val weight: Int) : SettingUiEvent
+    data class GenderSelected(val index: Int) : SettingUiEvent
+    data class ActivityLevelSelected(val index: Int) : SettingUiEvent
 }
