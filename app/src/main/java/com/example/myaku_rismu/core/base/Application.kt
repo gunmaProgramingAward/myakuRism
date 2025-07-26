5f1c02b2275a4676bd2274c93aee92b8fa0e9a39
Shunsuke Uchimura <25cm0105@jec.ac.jp>
2025-07-27 00:06:29 +0900

    命名の変更

--- app/src/main/java/com/example/myaku_rismu/core/base/Application.kt
+++ app/src/main/java/com/example/myaku_rismu/core/base/Application.kt
@@ -3,16 +3,11 @@ package com.example.myaku_rismu.core.base
 import android.app.Application
 import androidx.datastore.core.DataStore
 import androidx.datastore.preferences.core.Preferences
-import androidx.datastore.preferences.core.PreferenceDataStoreFactory
-import androidx.datastore.preferences.preferencesDataStoreFile
+import androidx.datastore.preferences.preferencesDataStore
 import dagger.hilt.android.HiltAndroidApp
 
 @HiltAndroidApp
 class MyakuRismuApplication : Application() {
-    val dataStore: DataStore<Preferences> by lazy {
-        PreferenceDataStoreFactory.create(
-            produceFile = { preferencesDataStoreFile("settings") }
-        )
-    }
+    val dataStore: DataStore<Preferences> by preferencesDataStore(name = "app_preferences")
 
 }
