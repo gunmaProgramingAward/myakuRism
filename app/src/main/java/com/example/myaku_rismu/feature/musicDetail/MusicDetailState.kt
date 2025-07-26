e9ee2a22876a9ae6c7f19f33b938e6560fbb363c
REOysd <202yyrr@gmail.com>
2025-07-26 17:25:11 +0900

    MusicDetailScreenの拡大縮小の実装

--- app/src/main/java/com/example/myaku_rismu/feature/musicDetail/MusicDetailState.kt
+++ app/src/main/java/com/example/myaku_rismu/feature/musicDetail/MusicDetailState.kt
@@ -4,7 +4,17 @@ import com.example.myaku_rismu.core.ScreenState
 
 data class MusicDetailState(
     val screenState: ScreenState = ScreenState.Initializing(),
-    var isPlaying: Boolean = false,
-    var isFavorite: Boolean = false,
+    val playerState: PlayerState = PlayerState.COLLAPSED,
+    val musicSliderPositon: Float = 0f,
+    val musicImagePausedRotation: Float = 0f,
+    val dragOffset: Float = 0f,
+    val isPlaying: Boolean = false,
+    val isFavorite: Boolean = false,
+    val isShuffle: Boolean = false,
 )
+
+enum class PlayerState {
+    COLLAPSED,
+    EXPANDED
+}
 
